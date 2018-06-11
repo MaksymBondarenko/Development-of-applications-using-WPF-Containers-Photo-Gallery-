8cc88c820802587786c28575265f9ad3e72baba9
MaksymBondarenko <maksymbondarenko1980@gmail.com>
2018-06-11 23:32:51 +0300

    Last changes 11.06.2018

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace WpfApp1
 {
@@ -20,9 +22,60 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Image IM = null;
+        BitmapImage btm = null;
+        Button btn = null;
+        List<String> paths = new List<String>();
+        System.Windows.Forms.FolderBrowserDialog FBI = new System.Windows.Forms.FolderBrowserDialog();
+
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (FBI.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                paths.AddRange(Directory.GetFiles(FBI.SelectedPath));
+
+                foreach (var item in paths)
+                {
+                    btm = new BitmapImage();
+                    btn = new Button();
+                    IM = new Image();
+                    btm.BeginInit();
+                    btm.UriSource = new Uri(item, UriKind.Absolute);
+                    btm.EndInit();
+                    IM.Source = btm;
+                    SP_1.Children.Add(btn);
+                    btn.Content = IM;
+                    btn.Click += Btn_Click;
+                }
+                
+            }
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            im_1.Source=((sender as Button).Content as Image).Source;
+            
+        }
+
+        private void EX_1_Expanded(object sender, RoutedEventArgs e)
+        {
+           
+            EX_1.Content = im_1.Source.ToString();   
+        }
+
+        private void EX_1_Collapsed(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
